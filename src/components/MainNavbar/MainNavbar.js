2b3b3f9bdaacda88b44159a1d8b7f13e03197353
Руслан <dewaft@icloud.com>
2022-09-10 18:32:03 +0300

    init

---
+++ src/components/MainNavbar/MainNavbar.js
@@ -0,0 +1,22 @@
+﻿// @vue/component
+export default {
+    name: 'main-navbar',
+
+    components: {},
+
+    mixins: [],
+
+    props: {},
+
+    data () {
+        return {}
+    },
+
+    computed: {},
+
+    watch: {},
+
+    created () {},
+
+    methods: {}
+}
---
+++ src/components/MainNavbar/MainNavbar.vue
@@ -0,0 +1,12 @@
+﻿<template>
+    <div class="main-navbar">
+      <h1>Современная школа</h1>
+      <div class="main-navbar__items">
+        <RouterLink to="/">Главная</RouterLink>
+        <RouterLink to="/about">Организации</RouterLink>
+      </div>
+      </div>
+</template>
+
+<script src="./MainNavbar.js" lang="js"></script>
+<style src="./MainNavbar.css" scoped></style>
