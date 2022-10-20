9b12e22d98f871d1434f6b2b1ce47f4b6e0f9e6f
exdeleter <exdeleter@gmail.com>
2022-10-20 21:58:25 +0300

    * добавил tailwind * изменил навбар

--- src/components/UI/MainNavbar/MainNavbar.vue
+++ src/components/UI/MainNavbar/MainNavbar.vue
@@ -3,32 +3,28 @@
 
 <template>
     <div class="main-navbar">
-      <div @click="$router.push('/')">
-        Современная школа
+      <div class="main-navbar__container">
+        <div class="flex items-center" @click="$router.push('/')">
+          <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Современная школа</span>
+        </div>
+        <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
+          <span class="sr-only">Open main menu</span>
+          <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
+        </button>
+        <div class="hidden w-full md:block md:w-auto" id="navbar-default">
+          <ul class="flex flex-col p-4 mt-4 bg-gray-50 rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+            <li>
+              <a href="#" class="main-navbar__btns" aria-current="page" @click="$router.push('/organizations')" >Организации</a>
+            </li>
+            <li>
+              <a href="#" class="main-navbar__btns" @click="$router.push('/about')" >О сайте</a>
+            </li>
+            <li>
+              <a href="#" class="main-navbar__btns" @click="$router.push('/teacher')">Учителю</a>
+            </li>
+          </ul>
+        </div>
       </div>
-        <ul class="complex_list">
-          <li>
-            <custom-button @click="$router.push('/organizations')"
-            >Организации
-            </custom-button>
-          </li>
-          <li>
-            <custom-button @click="$router.push('/about')"
-            >Ученику
-            </custom-button>
-          </li>
-          <li>
-            <custom-button @click="$router.push('/teacher')"
-            >Учителю
-            </custom-button>
-          </li>
-          <li>
-            <custom-button style="width: 100%"
-                @click="$router.push('/about')"
-            >О сайте
-            </custom-button>
-          </li>
-        </ul>
     </div>
 </template>
 
